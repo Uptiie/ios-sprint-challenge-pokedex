d82945d0c55737d21e02be803ba1c5534afce87d
Donovan Langager <donovan.lan16@gmail.com>
2019-09-06 14:36:22 -0700

    Added searchPokemon Outlets

---
+++ pokemon sprint/pokemon sprint/View Controllers/SearchViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SearchViewController.swift
+//  pokemon sprint
+//
+//  Created by Uptiie on 9/6/19.
+//  Copyright © 2019 Walcenberg, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var pokemonSearch: UISearchBar!
+    @IBAction func saveButton(_ sender: Any) {
+    }
+
+}
