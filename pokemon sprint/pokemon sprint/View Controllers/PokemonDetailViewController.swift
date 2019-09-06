b9815f7ddaac5331b17b159a9341861c5aae7cc0
Donovan Langager <donovan.lan16@gmail.com>
2019-09-06 14:57:53 -0700

    Pokemon DetailView Controller

--- pokemon sprint/pokemon sprint/View Controllers/PokemonDetailViewController.swift
+++ pokemon sprint/pokemon sprint/View Controllers/PokemonDetailViewController.swift
@@ -10,4 +10,32 @@ import UIKit
 
 class PokemonDetailViewController: UIViewController {
     
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    var pokemon: Pokemon? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+    }
+    
+    func updateViews() {
+        guard isViewLoaded,
+            let pokemon = pokemon else { return }
+        pokemonNameLabel.text = pokemon.name
+        idLabel.text = "\(pokemon.id)"
+        abilitiesLabel.text = pokemon.abilities
+        typeLabel.text = pokemon.type
+        guard let imageData = try? Data(contentsOf: pokemon.)
+        
+    }
  
+}
