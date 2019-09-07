ba760b3702ef5197f6942730b343f1d47ab4e243
Donovan Langager <donovan.lan16@gmail.com>
2019-09-07 16:30:31 -0700

    Created outlets and added update views to detailVC

--- Pokedex/Pokedex/View Controller/PokemonDetailViewController.swift
+++ Pokedex/Pokedex/View Controller/PokemonDetailViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class PokemonDetailViewController: UIViewController {
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var IDLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    var pokemon: Pokemon? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func updateViews() {
+        guard isViewLoaded,
+            let pokemon = pokemon else { return }
+        nameLabel.text = pokemon.name
+        IDLabel.text = "\(pokemon.id)"
+        typeLabel.text = "\(pokemon.types)"
+        abilitiesLabel.text = "\(pokemon.abilities)"
+        imageView.image = UIImage(data: pokemon.sprites.frontDefault)
     }
 
 }
